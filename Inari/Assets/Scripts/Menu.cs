22b925bc305ea116f7fa2b67e06ee52f1f756837
Isaac Harvey <ijharvey456@gmail.com>
2021-06-19 23:16:04 -0500

    Added TMPro and created main menu

---
+++ Inari/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private string mainSceneName;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => PlayGame());
+        quitButton.onClick.AddListener(() => QuitGame());
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(mainSceneName);
+    }
+
+    private void QuitGame() => Application.Quit();
+    
+}
