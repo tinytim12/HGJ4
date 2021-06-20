b8a30945529f9cb7836d5b997e07eba0be76d3ea
tinytim12 <evilsengokunadeko@gmail.com>
2021-06-20 14:31:25 +0800

    yarn
    Set up yarn

---
+++ Inari/Assets/Scripts/Person.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public YarnProgram[] yarnScripts;
+    // Start is called before the first frame update
+    void Start() {
+        transform.GetChild(0).transform.GetChild(0).GetComponent<DialogueRunner>().yarnScripts = yarnScripts;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
