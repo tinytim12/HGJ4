97fd924b3bff09a3ca7e4049eaa5816e3faf854c
Isaac Harvey <ijharvey456@gmail.com>
2021-06-24 16:04:17 -0500

    Added rotation camera movement

--- Inari/Assets/Scripts/Interaction/CameraMovement.cs
+++ Inari/Assets/Scripts/Interaction/CameraMovement.cs
@@ -17,12 +17,14 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float minZ;
     [SerializeField] private float maxZ;
     [SerializeField] private float dragSpeed;
+    [SerializeField] private float rotationSpeed = 1;
     // here just for testing and because it could be an option in the future
     [SerializeField] private bool controlsInverted;
+    [SerializeField] private Transform environment;
 
     private Camera _camera;
     private Vector3 _cameraDirection;
-    private Vector3 _dragOrigin;
+    private Vector2 _input;
     private void Awake()
     {   
         _camera = GetComponent<Camera>();
@@ -30,8 +32,11 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        _input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
         HandleZoom();
         HandleMovement();
+        HandleRotation();
     }
 
     // handles having a small amount of zoom for a feeling of greater immersion
@@ -45,18 +50,18 @@ public class CameraMovement : MonoBehaviour
         _camera.orthographicSize = Mathf.Clamp(newZoomSize, minimumCameraSize, maximumCameraSize);
     }
 
-    // right click to rotate around and left click to pan around a little bit
     private void HandleMovement()
     {
         // if the mouse is being held move the camera based on the original drag position and the current mouse position
         if(!Input.GetMouseButton(0)) return;
     
-        Vector2 movement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         // flip the axis around because of the orthographic rotation
-        _cameraDirection.z = -movement.x * dragSpeed;
-        _cameraDirection.x = movement.x * dragSpeed;
-        _cameraDirection.y = movement.y * dragSpeed;
+        _cameraDirection.z = -_input.x * dragSpeed;
+        _cameraDirection.x = _input.x * dragSpeed;
+        _cameraDirection.y = _input.y * dragSpeed;
+
+        _cameraDirection *= Time.deltaTime;
 
         transform.Translate(controlsInverted ? _cameraDirection : -_cameraDirection, Space.World);
 
@@ -65,5 +70,12 @@ public class CameraMovement : MonoBehaviour
         float z = Mathf.Clamp(transform.position.z, minZ, maxZ);
 
         transform.position = new Vector3(x, y, z);
+    }
+
+    private void HandleRotation()
+    {
+        if(!Input.GetMouseButton(1)) return;
+
+        environment.transform.Rotate(new Vector3(0, -_input.x * rotationSpeed, 0) * Time.deltaTime, Space.World);
     }
 }
