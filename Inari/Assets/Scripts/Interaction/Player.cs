45356ed8d6164afb171cfb56c6080c522bfcb736
Isaac Harvey <ijharvey456@gmail.com>
2021-06-24 11:22:22 -0500

    Fixed event system bug and mouse click bug

--- Inari/Assets/Scripts/Interaction/Player.cs
+++ Inari/Assets/Scripts/Interaction/Player.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using System.Linq;
 
 public class Player : MonoBehaviour
 {
-    public DialogueRunner dialogueRunner;
+    public DialogueRunner[] dialogueRunner;
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            if (!dialogueRunner.IsDialogueRunning)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(dialogueRunner.Any(x => x.IsDialogueRunning)) return;
 
-                if (Physics.Raycast(ray, out RaycastHit hit))
-                {
-                    Building building = hit.collider.GetComponentInParent<Building>();
-                    if (building) building.OnUserSelect();
-                }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Building building = hit.collider.GetComponentInParent<Building>();
+                if (building) building.OnUserSelect();
             }
             
         }
